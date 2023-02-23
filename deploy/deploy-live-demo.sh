6560ee8dc4ef8567999119fe156413cd82d9b878
Allan Miranda <allangxg@gmail.com>
2023-02-23 17:02:37 -0300

    update

--- deploy/deploy-live-demo.sh
+++ deploy/deploy-live-demo.sh
@@ -10,9 +10,9 @@ echo "😲 Warning! You are about to deploy to the live demo environment";
 read -n 1 -s -r -p "Press any key to continue, or ctrl+c to exit..."; 
 echo
 
-export AZURE_TENANT="72f988bf-86f1-41af-91ab-2d7cd011db47"
+export AZURE_TENANT="9334e33a-c607-4663-92e0-cc9eb3243cbe"
 export AZURE_SUB="62f51b53-9e7e-4673-8fcf-e312c82e113b"
-export GITHUB_REPO=https://github.com/benc-uk/chatr.git
+export GITHUB_REPO=https://github.com/allangxg/chatr
 export AZURE_REGION=centralus
 export AZURE_PREFIX=chatrlive
 export AZURE_RESGRP=chatr-live
